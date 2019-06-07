3e4155fa56c9b00ef8b86565521d76b7bbce7a3c
Felipe Sanabria <felipesanabriap@gmail.com>
2019-06-07 17:02:07 -0500

    Updates

---
+++ precache-manifest.e24ae7dc8bbf938f9f9015cb2722793d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e155153979a9d16e760563a84332fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1945f5c156b7a2d26d9c",
+    "url": "/static/css/main.c86590f5.chunk.css"
+  },
+  {
+    "revision": "0fbee43de2ce91351c3b",
+    "url": "/static/js/2.529cdfbf.chunk.js"
+  },
+  {
+    "revision": "1945f5c156b7a2d26d9c",
+    "url": "/static/js/main.cb1077bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
