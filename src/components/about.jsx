9cbe674440a2b5cf423891bd7fc6396bfff9640f
Felipe Sanabria <felipesanabriap@gmail.com>
2019-06-07 17:37:48 -0500

    added favicon, title, email link, whatsapp link

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,10 +11,10 @@ export default class About extends Component {
           I'm a Frontend Web Developer living in Bogotá, Colombia. I have 3+ years of experience working in several digital agencies and as a Freelance developer, where I tried to focus my work to create awesome websites, both in design and in function, using the lastest technologies. I have experience in React.js, Angular.js and Vue.js, as well as in tolls such as Wordpress and Webflow.
         </p>
         <p className="About-txt">
-          You can see a lot of my work on <a href="https://github.com/FelipeSanabria">Github</a>, or contact me through <a href="https://www.facebook.com/FelipeSanabria">Facebook</a> or <a href="https://www.linkedin.com/in/felipe-sanabria-a91538138/">Linkedin</a>.
+          You can see a lot of my work on <a href="https://github.com/FelipeSanabria" target="_blank" rel="noopener noreferrer">Github</a>, or contact me through <a href="https://www.facebook.com/FelipeSanabria" target="_blank" rel="noopener noreferrer">Facebook</a> or <a href="https://www.linkedin.com/in/felipe-sanabria-a91538138/" target="_blank" rel="noopener noreferrer">Linkedin</a>.
         </p>
         <p className="About-txt">
-          I am currently available for freelance and remote jobs. Let's talk.
+          I am currently available for freelance and remote jobs. If you have an offer or just want to talk to me about some really cool project, you can <a href="mailto:felipesanabriap@gmail.com">send me an email</a> or <a href="https://wa.me/573153376365" target="_blank" rel="noopener noreferrer">message me in Whatsapp</a>.
         </p>
       </div>
     )
